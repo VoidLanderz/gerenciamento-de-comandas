58f4fad9e6b19e32fb2baa6e3b6c84a935ed66d0
VoidLanderz <emanuelduraes1@gmail.com>
2024-11-11 21:56:03 -0300

    Atualizando as comandas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let spanRemovido = null;
-
 class Pedido {
     constructor(id, produto, quantidade, status ='pedente') {
     this.id = id;
@@ -18,12 +16,43 @@ class Restaurante {
     adicionarPedido(produto, quantidade) {
         const pedido = new Pedido(this.idAtual++, produto, quantidade);
         this.pedidos.push(pedido);
+        this.atualizarStorage();
         this.montarElementoProduto();
     }
 
+    atualizarPedido(id, status) {
+        const pedido = this.pedidos.find(pedido => pedido.id === id);
+        if(pedido) {
+            pedido.status = status;
+            this.atualizarStorage();
+            this.montarElementoProduto();
+        }
+    }
+
+    removerPedido(id) {
+        const index = this.pedidos.findIndex(pedido => pedido.id === id);
+        if(index != -1) {
+            this.pedidos.splice(index, 1 );
+            this.atualizarStorage();
+            this.montarElementoProduto();
+        }
+    }
+
+    atualizarStorage() {
+        localStorage.setItem("@pedidos", JSON.stringify(this.pedidos));
+    }
+
+    buscarStorage() {
+        const storagePedidos = localStorage.getItem("@pedidos");
+        if(storagePedidos) {
+            this.pedidos = JSON.parse(storagePedidos);
+            this.montarElementoProduto();
+        }
+    }
+
     montarElementoProduto() {
-        const listaPedidos = document.getElementById("lista-pedidos")
-        listaPedidos.innerHTML =" "
+        const listaPedidos = document.getElementById("lista-pedidos");
+        listaPedidos.innerHTML ="";
 
         this.pedidos.forEach((pedido) => {
             const li = document.createElement("li");
@@ -33,9 +62,9 @@ class Restaurante {
             <p><strong>Quantidade:</strong>${pedido.quantidade}</p>
             <p><strong>Status:</strong>${pedido.status}</p>
             <div class="acao-pedido">
-                <button class="btn-atualizar">Em preparo</button>
-                <button class="btn-atualizar">Finalizado</button>
-                <button class="btn-remover">Remover</button>
+                <button class="btn-atualizar" onClick ="atualizarStatus(${pedido.id}, 'em preparo')">Em preparo</button>
+                <button class="btn-atualizar" onClick ="atualizarStatus(${pedido.id}, 'finalizado')">Finalizado</button>
+                <button class="btn-remover" onClick ="removerPedido(${pedido.id})">Remover</button>
             </div>`
             ;
             listaPedidos.appendChild(li);
@@ -44,6 +73,14 @@ class Restaurante {
 }
 
 const restaurante = new Restaurante();
+
+function iniciarDados() {
+    restaurante.buscarStorage();
+
+    const lista = document.getElementById("lista-pedidos");
+    this.montarElementoNaoExiste(lista);
+}
+
 function adicionarPedido() {
     const produto = document.getElementById("produto").value;
     const quantidade = document.getElementById("quantidade").value;
@@ -53,26 +90,47 @@ function adicionarPedido() {
         document.getElementById("produto").value = "";
         document.getElementById("quantidade").value = "";
     
-    atualizarMensagem();
-    
-    } else {
-        alert ('você não preencheu todos os campos!')
-    }
+        atualizarMensagem();
+        } else {
+            alert ('você não preencheu todos os campos!')
+        }
+}
+
+function atualizarStatus(id, status) {
+    restaurante.atualizarPedido(id, status);
+}
+
+function removerPedido(id) {
+    restaurante.removerPedido(id);
+    this.atualizarMensagem();
 }
 
 function atualizarMensagem() {
-    const lista = document.getElementById("lista-pedidos")
-    
+    const lista = document.getElementById("lista-pedidos");
+    let mensagemNaoExiste = document.getElementById("nao-existe");
+
     if(lista.children.length === 0) {
-        if (spanRemovido) {
-            const lista = document.querySelector(".lista-pedidos").insertBefore(spanRemovido, document.querySelector(".lista-pedidos").firstChild);
-                spanRemovido = null;
-        }
+        this.montarElementoNaoExiste(lista);
     } else {
-        let mensagemNaoExiste = document.getElementById("nao-existe");
         if(mensagemNaoExiste) {
-            spanRemovido = mensagemNaoExiste;
             mensagemNaoExiste.remove();
         }
     }
 } 
+
+function montarElementoNaoExiste(lista) {
+    let mensagemNaoExiste = document.getElementById('nao-existe')
+
+    if (!mensagemNaoExiste && lista.children.length === 0) {
+        {
+            const li = document.createElement("li")
+            const mensagem = document.createElement("span");
+            mensagem.id = "nao-existe";
+            mensagem.textContent = "Não existe pedidos";
+            lista.appendChild(li);
+            li.appendChild(mensagem);
+        }
+    }
+}
+
+this.iniciarDados();
